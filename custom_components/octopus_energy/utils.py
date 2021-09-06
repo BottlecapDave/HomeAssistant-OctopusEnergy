9c4dc2dbc35afddd051213e384ff5fe9771d9418
Stuart Chapman <StuBez@users.noreply.github.com>
2021-09-06 12:05:46 +0100

    Use dt.parse_datetime for agreement dates
    `datetime.fromisoformat` is only intended as the inverse of
    `datetime.isoformat` and does not support parsing arbitrary ISO-8601
    strings.
    
    Home Assistant provide `dt.parse_datetime` which seems to be an
    appropriate substitute.

--- custom_components/octopus_energy/utils.py
+++ custom_components/octopus_energy/utils.py
@@ -1,16 +1,13 @@
-from datetime import datetime
-from homeassistant.util.dt import (utcnow, as_utc)
+from homeassistant.util.dt import (utcnow, as_utc, parse_datetime)
 
 def get_active_agreement(agreements):
-  active_agreement = None
   now = utcnow()
 
   for agreement in agreements:
-    valid_from = as_utc(datetime.fromisoformat(agreement["valid_from"]))
-    valid_to = as_utc(datetime.fromisoformat(agreement["valid_to"]))
+    valid_from = as_utc(parse_datetime(agreement["valid_from"]))
+    valid_to = as_utc(parse_datetime(agreement["valid_to"]))
 
     if valid_from <= now and valid_to >= now:
-      active_agreement = agreement
-      break
+      return agreement
   
-  return active_agreement
+  return None
