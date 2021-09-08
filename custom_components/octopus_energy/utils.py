0d50a45327b46042269d3605b464024952040c54
BottlecapDave <me@davidskendall.co.uk>
2021-09-08 20:06:18 +0100

    fix: Updated get_active_agreement to handle agreements with no end in sight

--- custom_components/octopus_energy/utils.py
+++ custom_components/octopus_energy/utils.py
@@ -4,10 +4,15 @@ def get_active_agreement(agreements):
   now = utcnow()
 
   for agreement in agreements:
+    
     valid_from = as_utc(parse_datetime(agreement["valid_from"]))
-    valid_to = as_utc(parse_datetime(agreement["valid_to"]))
-
-    if valid_from <= now and valid_to >= now:
+    
+    valid_to = None
+    if "valid_to" in agreement:
+      valid_to = as_utc(parse_datetime(agreement["valid_to"]))
+    
+    # Some agreements have no end date, which means they are still active with no end in sight
+    if valid_from <= now and (valid_to == None or valid_to >= now):
       return agreement
   
   return None
