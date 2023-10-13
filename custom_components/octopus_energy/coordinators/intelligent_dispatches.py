1ef6147f4f390e20b1b7c7cd39c446d77e0315ef
Gavin Melling <gavin@pixelinspiration.com>
2023-10-13 21:28:52 +0100

    Updated intelligent_dispatches to timedelta minutes

--- custom_components/octopus_energy/coordinators/intelligent_dispatches.py
+++ custom_components/octopus_energy/coordinators/intelligent_dispatches.py
@@ -80,7 +80,7 @@ async def async_setup_intelligent_dispatches_coordinator(hass, account_id: str):
     update_method=async_update_intelligent_dispatches_data,
     # Because of how we're using the data, we'll update every minute, but we will only actually retrieve
     # data every 30 minutes
-    update_interval=timedelta(minutes=COORDINATOR_REFRESH_IN_SECONDS),
+    update_interval=timedelta(seconds=COORDINATOR_REFRESH_IN_SECONDS),
     always_update=True
   )
 
