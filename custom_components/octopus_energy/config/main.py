f5fbc0cea9b4f65692a2998c3a3f340ebe65f909
BottlecapDave <me@davidskendall.co.uk>
2023-10-21 15:48:57 +0100

    fix: Fixed clearing of price caps without needing checkbox

--- custom_components/octopus_energy/config/main.py
+++ custom_components/octopus_energy/config/main.py
@@ -1,6 +1,8 @@
 from ..const import (
   CONFIG_MAIN_ACCOUNT_ID,
   CONFIG_MAIN_API_KEY,
+  CONFIG_MAIN_ELECTRICITY_PRICE_CAP,
+  CONFIG_MAIN_GAS_PRICE_CAP,
   CONFIG_MAIN_LIVE_ELECTRICITY_CONSUMPTION_REFRESH_IN_MINUTES,
   CONFIG_MAIN_LIVE_GAS_CONSUMPTION_REFRESH_IN_MINUTES,
   CONFIG_MAIN_PREVIOUS_ELECTRICITY_CONSUMPTION_DAYS_OFFSET,
@@ -8,6 +10,22 @@ from ..const import (
   CONFIG_MAIN_SUPPORTS_LIVE_CONSUMPTION
 )
 from ..api_client import OctopusEnergyApiClient
+
+def merge_main_config(data: dict, options: dict, updated_config: dict = None):
+  config = dict(data)
+  if options is not None:
+    config.update(options)
+
+  if updated_config is not None:
+    config.update(updated_config)
+
+    if CONFIG_MAIN_ELECTRICITY_PRICE_CAP not in updated_config and CONFIG_MAIN_ELECTRICITY_PRICE_CAP in config:
+      del config[CONFIG_MAIN_ELECTRICITY_PRICE_CAP]
+
+    if CONFIG_MAIN_GAS_PRICE_CAP not in updated_config and CONFIG_MAIN_GAS_PRICE_CAP in config:
+      del config[CONFIG_MAIN_GAS_PRICE_CAP]
+
+  return config
 
 async def async_validate_main_config(data):
   errors = {}
