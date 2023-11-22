350ad20ccbae8c0e4360053d2af8bb4098e833d7
BottlecapDave <me@davidskendall.co.uk>
2023-11-22 18:28:55 +0000

    feat: Added octopoints_per_kwh to joined saving session events (if known)

--- tests/unit/coordinators/test_async_refresh_saving_sessions.py
+++ tests/unit/coordinators/test_async_refresh_saving_sessions.py
@@ -33,6 +33,7 @@ def assert_raised_new_saving_session_event(
 def assert_raised_all_saving_session_event(
   raised_event: dict,
   account_id: str,
+  all_available_events: list[SavingSession],
   available_events: list[SavingSession],
   joined_events: list[SavingSession]
 ):
@@ -72,6 +73,16 @@ def assert_raised_all_saving_session_event(
     
     assert "rewarded_octopoints" in actual_event
     assert actual_event["rewarded_octopoints"] == expected_event.octopoints
+
+    expected_available_event = None
+    for event in all_available_events:
+      if event.id == expected_event.id:
+        expected_available_event = event
+        break
+
+    assert expected_available_event is not None
+    assert "octopoints_per_kwh" in actual_event
+    assert actual_event["octopoints_per_kwh"] == expected_available_event.octopoints
 
 @pytest.mark.asyncio
 async def test_when_now_is_not_at_30_minute_mark_and_previous_data_is_available_then_previous_data_returned():
@@ -146,7 +157,7 @@ async def test_when_upcoming_events_contains_events_in_past_then_events_filtered
     assert result is not None
 
     assert len(actual_fired_events) == 1
-    assert_raised_all_saving_session_event(actual_fired_events[EVENT_ALL_SAVING_SESSIONS], account_id, [], [])
+    assert_raised_all_saving_session_event(actual_fired_events[EVENT_ALL_SAVING_SESSIONS], account_id, [], [], [])
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize("minutes",[
@@ -187,7 +198,7 @@ async def test_when_upcoming_events_contains_joined_events_then_events_filtered_
     assert result is not None
 
     assert len(actual_fired_events) == 1
-    assert_raised_all_saving_session_event(actual_fired_events[EVENT_ALL_SAVING_SESSIONS], account_id, [], [expected_saving_session])
+    assert_raised_all_saving_session_event(actual_fired_events[EVENT_ALL_SAVING_SESSIONS], account_id, [expected_saving_session], [], [expected_saving_session])
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize("minutes",[
@@ -229,7 +240,7 @@ async def test_when_upcoming_events_present_and_no_previous_data_then_new_event_
 
     assert len(actual_fired_events) == 2
     assert_raised_new_saving_session_event(actual_fired_events[EVENT_NEW_SAVING_SESSION], account_id, expected_saving_session)
-    assert_raised_all_saving_session_event(actual_fired_events[EVENT_ALL_SAVING_SESSIONS], account_id, [expected_saving_session], [])
+    assert_raised_all_saving_session_event(actual_fired_events[EVENT_ALL_SAVING_SESSIONS], account_id, [expected_saving_session], [expected_saving_session], [])
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize("minutes",[
@@ -271,7 +282,7 @@ async def test_when_upcoming_events_present_and_not_in_previous_data_then_new_ev
 
     assert len(actual_fired_events) == 2
     assert_raised_new_saving_session_event(actual_fired_events[EVENT_NEW_SAVING_SESSION], account_id, expected_saving_session)
-    assert_raised_all_saving_session_event(actual_fired_events[EVENT_ALL_SAVING_SESSIONS], account_id, [expected_saving_session], [])
+    assert_raised_all_saving_session_event(actual_fired_events[EVENT_ALL_SAVING_SESSIONS], account_id, [expected_saving_session], [expected_saving_session], [])
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize("minutes",[
@@ -313,7 +324,7 @@ async def test_when_upcoming_events_present_and_in_previous_data_then_new_event_
     assert result is not None
 
     assert len(actual_fired_events) == 1
-    assert_raised_all_saving_session_event(actual_fired_events[EVENT_ALL_SAVING_SESSIONS], account_id, [expected_saving_session], [])
+    assert_raised_all_saving_session_event(actual_fired_events[EVENT_ALL_SAVING_SESSIONS], account_id, [expected_saving_session], [expected_saving_session], [])
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize("minutes",[
@@ -356,7 +367,7 @@ async def test_when_upcoming_events_present_and_in_previous_data_but_with_differ
 
     assert len(actual_fired_events) == 2
     assert_raised_new_saving_session_event(actual_fired_events[EVENT_NEW_SAVING_SESSION], account_id, expected_saving_session)
-    assert_raised_all_saving_session_event(actual_fired_events[EVENT_ALL_SAVING_SESSIONS], account_id, [expected_saving_session], [])
+    assert_raised_all_saving_session_event(actual_fired_events[EVENT_ALL_SAVING_SESSIONS], account_id, [expected_saving_session], [expected_saving_session], [])
 
 @pytest.mark.asyncio
 async def test_when_previous_data_is_out_of_date_then_new_date_is_retrieved():
