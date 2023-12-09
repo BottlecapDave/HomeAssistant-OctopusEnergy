20a1a8c322dda994e3a5cd51222d5c346013702e
BottlecapDave <me@davidskendall.co.uk>
2023-12-09 15:34:24 +0000

    fix: Fixed issue with target rate when offset is applied twice to next time value

--- tests/unit/target_rates/test_get_target_rate_info.py
+++ tests/unit/target_rates/test_get_target_rate_info.py
@@ -212,7 +212,7 @@ async def test_when_offset_set_then_active_at_correct_current_time():
   ]
 
   # Check where we're before the offset
-  current_date = rates[0]["start"] - timedelta(hours=1, minutes=1)
+  current_date = datetime.strptime("2022-02-09T08:59:00Z", "%Y-%m-%dT%H:%M:%S%z")
 
   result = get_target_rate_info(
     current_date,
@@ -222,7 +222,7 @@ async def test_when_offset_set_then_active_at_correct_current_time():
 
   assert result is not None
   assert result["is_active"] == False
-  assert result["next_time"] == datetime.strptime("2022-02-09T08:00:00Z", "%Y-%m-%dT%H:%M:%S%z")
+  assert result["next_time"] == datetime.strptime("2022-02-09T09:00:00Z", "%Y-%m-%dT%H:%M:%S%z")
 
   assert result["overall_average_cost"] == 10
   assert result["overall_min_cost"] == 5
@@ -263,7 +263,7 @@ async def test_when_offset_set_then_active_at_correct_current_time():
     assert result["next_max_cost"] == 5
 
   # Check when within rate but after offset
-  current_date = rates[0]["start"] - timedelta(hours=1) + timedelta(minutes=61)
+  current_date = rates[0]["start"] + timedelta(minutes=1)
 
   result = get_target_rate_info(
     current_date,
@@ -273,7 +273,7 @@ async def test_when_offset_set_then_active_at_correct_current_time():
 
   assert result is not None
   assert result["is_active"] == False
-  assert result["next_time"] == datetime.strptime("2022-02-09T10:00:00Z", "%Y-%m-%dT%H:%M:%S%z")
+  assert result["next_time"] == datetime.strptime("2022-02-09T11:00:00Z", "%Y-%m-%dT%H:%M:%S%z")
 
   assert result["overall_average_cost"] == 10
   assert result["overall_min_cost"] == 5
