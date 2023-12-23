2cbf20caf4ff69dbac10d58f0e29d25e3a09a94d
BottlecapDave <me@davidskendall.co.uk>
2023-12-23 20:30:00 +0000

    chore: Fixed adding consumption data

--- custom_components/octopus_energy/cost_tracker/__init__.py
+++ custom_components/octopus_energy/cost_tracker/__init__.py
@@ -1,16 +1,15 @@
 from datetime import datetime, timedelta
 
-def calculate_consumption(current: datetime, consumption_data: list, new_value: float, old_value: float, is_accumulative_value):
+def add_consumption(current: datetime, consumption_data: list, new_value: float, old_value: float, is_accumulative_value):
   start_of_day = current.replace(hour=0, minute=0, second=0, microsecond=0)
   target_start = current.replace(minute=0 if current.minute % 30 == 0 else 30, second=0, microsecond=0)
   target_end = target_start + timedelta(minutes=30)
 
   # If we've gone into a new day, then reset the consumption result
   new_consumption_data = consumption_data
-  if consumption_data is not None and len(consumption_data) > 0 and consumption_data[0]["start"] != start_of_day:
+  if (consumption_data is not None and len(consumption_data) > 0 and
+      (consumption_data[0]["start"].year != start_of_day.year or consumption_data[0]["start"].month != start_of_day.month or consumption_data[0]["start"].day != start_of_day.day)):
     new_consumption_data = []
-
-  
 
   if is_accumulative_value == False:
     value = new_value
