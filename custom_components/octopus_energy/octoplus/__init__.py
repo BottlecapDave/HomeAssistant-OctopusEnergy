7fbbd7d339e1f4f22e862408c1e644966dc8ee1e
BottlecapDave <me@davidskendall.co.uk>
2023-12-31 14:16:12 +0000

    feat: Added function to retrieve target consumption dates for saving session target calculations

--- custom_components/octopus_energy/octoplus/__init__.py
+++ custom_components/octopus_energy/octoplus/__init__.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, timedelta
 from ..api_client.saving_sessions import SavingSession
 
 def current_saving_sessions_event(current_date: datetime, events: list[SavingSession]) -> SavingSession or None:
@@ -18,3 +18,42 @@ def get_next_saving_sessions_event(current_date: datetime, events: list[SavingSe
           next_event = event
 
   return next_event
+
+class SavingSessionConsumptionDate:
+  start: datetime
+  end: datetime
+
+  def __init__(self, start: datetime, end: datetime):
+    self.start = start
+    self.end = end
+
+def is_new_saving_session_date_valid(saving_session_start: datetime, previous_saving_sessions: list):
+  start_of_day = saving_session_start.replace(hour=0, minute=0, second=0, microsecond=0)
+  end_of_day = start_of_day + timedelta(days=1)
+  for saving_session in previous_saving_sessions:
+    if saving_session["start"] >= start_of_day and saving_session["start"] <= end_of_day:
+      return False
+  
+  return True
+
+def get_saving_session_consumption_dates(saving_session_start: datetime, saving_session_end: datetime, previous_saving_sessions: list) -> list[SavingSessionConsumptionDate]:
+  dates: list[SavingSessionConsumptionDate] = []
+
+  hours = saving_session_end - saving_session_start
+  saving_session_day = saving_session_start.weekday()
+  if (saving_session_day >= 5):
+    # Weekend
+    new_start = saving_session_start
+    while len(dates) < 4:
+      new_start = new_start - timedelta(days=1)
+      if (is_new_saving_session_date_valid(new_start, previous_saving_sessions) and new_start.weekday() >= 5):
+        dates.append(SavingSessionConsumptionDate(new_start, new_start + hours))
+  else:
+    # Weekday
+    new_start = saving_session_start
+    while len(dates) < 10:
+      new_start = new_start - timedelta(days=1)
+      if (is_new_saving_session_date_valid(new_start, previous_saving_sessions) and new_start.weekday() < 5):
+        dates.append(SavingSessionConsumptionDate(new_start, new_start + hours))
+
+  return dates
---
+++ tests/unit/octoplus/test_get_saving_session_consumption_dates.py
@@ -0,0 +1,256 @@
+from datetime import datetime, timedelta
+import pytest
+
+from custom_components.octopus_energy.octoplus import get_saving_session_consumption_dates
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("saving_session_start,expected_result",[
+  (
+    # Sunday
+    datetime.strptime("2023-12-31T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    [
+      datetime.strptime("2023-12-30T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-24T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-23T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-17T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    ]
+  ),
+  (
+    # Saturday
+    datetime.strptime("2023-12-30T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    [
+      datetime.strptime("2023-12-24T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-23T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-17T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-16T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    ]
+  ),
+  (
+    # Friday
+    datetime.strptime("2023-12-29T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    [
+      datetime.strptime("2023-12-28T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-27T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-26T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-25T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-22T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-21T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-20T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-19T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-18T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-15T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    ]
+  ),
+  (
+    # Thursday
+    datetime.strptime("2023-12-28T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    [
+      datetime.strptime("2023-12-27T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-26T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-25T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-22T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-21T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-20T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-19T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-18T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-15T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-14T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    ]
+  ),
+  (
+    # Wednesday
+    datetime.strptime("2023-12-27T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    [
+      datetime.strptime("2023-12-26T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-25T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-22T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-21T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-20T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-19T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-18T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-15T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-14T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-13T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    ]
+  ),
+  (
+    # Tuesday
+    datetime.strptime("2023-12-26T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    [
+      datetime.strptime("2023-12-25T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-22T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-21T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-20T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-19T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-18T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-15T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-14T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-13T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-12T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    ]
+  ),
+  (
+    # Monday
+    datetime.strptime("2023-12-25T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    [
+      datetime.strptime("2023-12-22T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-21T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-20T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-19T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-18T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-15T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-14T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-13T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-12T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-11T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    ]
+  )
+])
+async def test_when_no_previous_saving_sessions_then_correct_dates_returned(saving_session_start: datetime, expected_result: list[datetime]):
+  # Arrange
+  saving_session_diff = timedelta(minutes=90)
+  saving_session_end = saving_session_start + saving_session_diff
+  previous_saving_sessions = []
+
+  # Act
+  result = get_saving_session_consumption_dates(saving_session_start, saving_session_end, previous_saving_sessions)
+
+  # Assert
+  assert len(result) == len(expected_result)
+  for index in range(len(expected_result)):
+    expected_start = expected_result[index]
+    expected_end = expected_start + saving_session_diff
+
+    assert result[index].start == expected_start
+    assert result[index].end == expected_end
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("saving_session_start,previous_saving_session_start,expected_result",[
+  (
+    # Sunday
+    datetime.strptime("2023-12-31T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    datetime.strptime("2023-12-24T12:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    [
+      datetime.strptime("2023-12-30T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-23T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-17T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-16T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    ]
+  ),
+  (
+    # Saturday
+    datetime.strptime("2023-12-30T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    datetime.strptime("2023-12-23T12:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    [
+      datetime.strptime("2023-12-24T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-17T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-16T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-10T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    ]
+  ),
+  (
+    # Friday
+    datetime.strptime("2023-12-29T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    datetime.strptime("2023-12-26T12:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    [
+      datetime.strptime("2023-12-28T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-27T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-25T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-22T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-21T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-20T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-19T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-18T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-15T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-14T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    ]
+  ),
+  (
+    # Thursday
+    datetime.strptime("2023-12-28T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    datetime.strptime("2023-12-25T12:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    [
+      datetime.strptime("2023-12-27T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-26T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-22T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-21T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-20T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-19T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-18T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-15T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-14T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-13T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    ]
+  ),
+  (
+    # Wednesday
+    datetime.strptime("2023-12-27T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    datetime.strptime("2023-12-22T12:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    [
+      datetime.strptime("2023-12-26T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-25T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-21T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-20T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-19T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-18T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-15T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-14T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-13T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-12T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    ]
+  ),
+  (
+    # Tuesday
+    datetime.strptime("2023-12-26T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    datetime.strptime("2023-12-21T12:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    [
+      datetime.strptime("2023-12-25T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-22T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-20T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-19T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-18T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-15T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-14T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-13T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-12T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-11T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    ]
+  ),
+  (
+    # Monday
+    datetime.strptime("2023-12-25T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    datetime.strptime("2023-12-20T12:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    [
+      datetime.strptime("2023-12-22T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-21T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-19T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-18T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-15T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-14T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-13T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-12T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-11T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+      datetime.strptime("2023-12-08T17:00:00+01:00", "%Y-%m-%dT%H:%M:%S%z"),
+    ]
+  )
+])
+async def test_when_previous_saving_sessions_present_then_returned_dates_do_not_include_saving_session(saving_session_start: datetime, previous_saving_session_start: datetime, expected_result: list[datetime]):
+  # Arrange
+  saving_session_diff = timedelta(minutes=90)
+  saving_session_end = saving_session_start + saving_session_diff
+  previous_saving_sessions = [{
+    "start": previous_saving_session_start,
+    "end": previous_saving_session_start + timedelta(hours=1)
+  }]
+
+  # Act
+  result = get_saving_session_consumption_dates(saving_session_start, saving_session_end, previous_saving_sessions)
+
+  # Assert
+  assert len(result) == len(expected_result)
+  for index in range(len(expected_result)):
+    expected_start = expected_result[index]
+    expected_end = expected_start + saving_session_diff
+
+    assert result[index].start == expected_start
+    assert result[index].end == expected_end
