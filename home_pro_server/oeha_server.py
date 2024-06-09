8f9a4f058ba960fd7597bc7e6d4e114f9509d1dc
BottlecapDave <me@davidskendall.co.uk>
2024-06-09 09:57:00 +0100

    chore: Updated home pro server to have required auth header

--- home_pro_server/oeha_server.py
+++ home_pro_server/oeha_server.py
@@ -5,12 +5,20 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import requests
 
 han_host = os.getenv("HAN_API_HOST")
+auth_token = os.getenv("SERVER_AUTH_TOKEN")
 
 class RequestHandler(BaseHTTPRequestHandler):
   # Handle GET requests
   def do_GET(self):
     query = urlparse(self.path).query
     params = dict(qc.split("=") for qc in query.split("&"))
+
+    auth_header = self.headers.getheader('Authorization')
+    if auth_header == None or auth_header != auth_token:
+      self.send_response(401)
+      self.send_header('Content-type', 'text/html')
+      self.end_headers()
+      return
 
     if self.path.startswith("/get_meter_consumption"):
       # Set response status code
