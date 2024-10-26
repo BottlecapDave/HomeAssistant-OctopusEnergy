5bd1fa0134dae0cd49386b784a883129b858f0bf
BottlecapDave <me@davidskendall.co.uk>
2024-10-26 19:59:36 +0100

    chore: Fixed issues with importing cost/consumption data when no data is available

--- custom_components/octopus_energy/statistics/cost.py
+++ custom_components/octopus_energy/statistics/cost.py
@@ -98,7 +98,7 @@ async def async_import_external_statistics_from_cost(
         peak_statistics
       )
 
-      peak_totals[peak_type] = peak_statistics[-1]["sum"] if peak_statistics[-1] is not None else 0
+      peak_totals[peak_type] = peak_statistics[-1]["sum"] if len(peak_statistics) > 0 and peak_statistics[-1] is not None else 0
 
   return ImportCostStatisticsResult(statistics[-1]["sum"] if statistics[-1] is not None else 0,
                                     peak_totals)
