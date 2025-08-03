501d57cc77f066d617d2d5385043234758eb8bb0
David Kendall <me@davidskendall.co.uk>
2025-08-03 08:33:24 +0100

    feat: Moved update of configuration to "reconfigure" option to reduce bugs and follow HA practices (2 hours dev time)

--- custom_components/octopus_energy/number.py
+++ custom_components/octopus_energy/number.py
@@ -25,9 +25,6 @@ async def async_setup_entry(hass, entry, async_add_entities):
 
   config = dict(entry.data)
 
-  if entry.options:
-    config.update(entry.options)
-
   entities = []
 
   if CONFIG_MAIN_API_KEY in entry.data:
