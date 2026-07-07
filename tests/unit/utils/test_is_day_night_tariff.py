80faa43fc06bfda3a86fd0d269b1f363a6a72491
s-ducks <stephen.bias@krakenflex.com>
2026-07-08 00:08:50 +0100

    test: change example tariff codes

--- tests/unit/utils/test_is_day_night_tariff.py
+++ tests/unit/utils/test_is_day_night_tariff.py
@@ -8,7 +8,7 @@ from custom_components.octopus_energy.utils import is_day_night_tariff
   ("BUS-PANELP-12M-FIXED-SEPTEMBER2029", False),
   ("E-2R-VAR-22-11-01-A", True),
   ("E-FLAT2R-SILVER-23-12-06-A", True),
-  ("E-1R-IOG-SMB-TOU-25-12-12-A", True),
+  ("E-1R-IOG-SMB-VAR-24-10-29-A", True),
 ])
 async def test_when_tariff_code_provided_then_expected_result_returned(tariff_code: str, expected_result: bool):
   # Act
